f4cc215d1017e2a8185a63195d727991ac7d4a7d
Oleksandr Isaiev <nevkluchat@gmail.com>
2014-09-01 14:39:44 +0300

    Initial Commit

---
+++ ColloQR/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ColloQR
+//
+//  Created by Oleksandr Isaiev on 01.09.14.
+//  Copyright (c) 2014 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
